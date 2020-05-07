7da0c21e5536adc3dfaa322153d1ffefcd99593c
Evgeny Barbashov <ebarbashov@linkedin.com>
2020-05-07 13:27:02 -0700

    Change dependency to pytho3

--- Formula/remote.rb
+++ Formula/remote.rb
@@ -6,7 +6,7 @@ class Remote < Formula
   url "https://files.pythonhosted.org/packages/7f/b7/07c84eee90712f789512e737ba1fe5df0f6daee0045f223ad20997323ee1/remote-exec-1.3.7.tar.gz"
   sha256 "c2b0ea5d55476092324015b6297bf7b5417356b9503f9d1ecd7571ab874d3259"
 
-  depends_on "python@3.8"
+  depends_on "python3"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/27/6f/be940c8b1f1d69daceeb0032fee6c34d7bd70e3e649ccac0951500b4720e/click-7.1.2.tar.gz"
